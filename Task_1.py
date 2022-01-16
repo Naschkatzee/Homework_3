3b87ac888dc37ff7e13683bc7e0ecf8691ec7126
Naschkatzee <tatsiana.shelepen@gmail.com>
2022-01-16 12:51:24 +0300

    Task_1

--- Task_1.py
+++ Task_1.py
@@ -1,18 +1,17 @@
-while True:
-    try:
-        a = list(map(int, input('Значения одного катета n треугольников: ').split()))
-        b = list(map(int, input('Значение второго катета n треугольников: ').split()))
-        if len(a) == len(b):
-            index = 0
-            num = 1
-            while index < len(a) and len(b):
-                c = int((a[index]**2 + b[index]**2) ** 0.5)
-                S = int(a[index]*b[index]/2)
-                print(f'Треугольник {num} с катетами {a[index]} и {b[index]} имеет площадь {S} и гипотенузу {c}\n')
-                index += 1
-                num += 1
-        else:
-            print('InconsistentDataError')
-        break
-    except ValueError:
+class InconsistentDataError (Exception):
+    pass
+
+try:
+    a = list(map(int, input('Значения одного катета n треугольников: ').split()))
+    b = list(map(int, input('Значение второго катета n треугольников: ').split()))
+    num = 0
+    if len(a) == len(b):
+        for index in range (len(a)) and range (len(b)):
+            c = int((a[index]**2 + b[index]**2) ** 0.5)
+            S = int(a[index]*b[index]/2)
+            num += 1
+            print(f'Треугольник {num} с катетами {a[index]} и {b[index]} имеет площадь {S} и гипотенузу {c}\n')
+    else:
+        raise InconsistentDataError ('Неравные массивы')
+except ValueError:
         print('NonNumericError')
