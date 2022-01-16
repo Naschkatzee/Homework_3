2db78c2d00837ef894498894df5cbc0d815a3089
Naschkatzee <tatsiana.shelepen@gmail.com>
2022-01-16 12:51:58 +0300

    Task_1 (part 2)

--- Task_1_2.py
+++ Task_1_2.py
@@ -1,14 +1,20 @@
-d = list (map(int, input ("Введите несколько значений 2-х катетов через пробел: "). split ()))
-if len(d) % 2 == 0:
-    item_a = 0
-    item_b = 1
+class InconsistentDataError (Exception):
+    pass
+
+try:
+    d = list (map(int, input ("Введите несколько значений 2-х катетов через пробел: "). split ()))
+    a = d[0::2]
+    b = d[1::2]
     number = 1
-    while item_a < len (d):
-        e = int ((d[item_a]**2 + d[item_b]**2) ** 0.5)
-        s = int (d[item_a]*d[item_b]/2)
-        print (f'Треугольник {number} с катетами {d[item_a]} и {d[item_b]} имеет площадь {s} и гипотенузу {e} \n')
-        item_a += 2
-        item_b += 2
-        number += 1 
-else:
-    print ('Вы ввели нечетное количество катетов.')
+    if len(d) % 2 == 0:
+        for index in range (len(a)) and range (len(b)):
+            e = int((a[index]**2 + b[index]**2) ** 0.5)
+            s = int(a[index] * b[index]/2)
+            print (f'Треугольник {number} с катетами {a[index]} и {b[index]} имеет площадь {s} и гипотенузу {e} \n')
+            number += 1 
+    else:
+        raise InconsistentDataError ('Вы ввели нечетное количество катетов')
+except ValueError:
+        print('NonNumericError')
+
+    
